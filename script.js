09655ba74bd3341b9d79e793dd31305344424a46
angel <delusional@outlook.jp>
2024-08-08 07:15:19 +0200

    Split Index into > Script.js + Styles.css, changed the fonts, layout, colors website navbar

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.animated-section');
+    const navbar = document.querySelector('.custom-navbar');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
